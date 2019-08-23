df0c43d8e197a14a49d0be433544f31c220759f6
jdfrozen <616769416@qq.com>
2019-08-23 11:04:59 +0800

    修正工程再处理

---
+++ zjjw-server/src/main/java/com/zjjw/zjjwserver/ZjjwServerApplication.java
@@ -0,0 +1,13 @@
+package com.zjjw.zjjwserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZjjwServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZjjwServerApplication.class, args);
+    }
+
+}
