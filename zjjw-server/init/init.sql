10956feb8ae68e6b0a0800bda624fd1b3138a266
jdfrozen <616769416@qq.com>
2019-09-02 20:27:30 +0800

    增加通信模块

--- zjjw-server/init/init.sql
+++ zjjw-server/init/init.sql
@@ -1,17 +1,18 @@
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
   `name` varchar(32) DEFAULT NULL COMMENT '姓名',
   `phone` char(11) DEFAULT NULL COMMENT '手机号码',
   `telephone` varchar(16) DEFAULT NULL COMMENT '住宅电话',
   `address` varchar(64) DEFAULT NULL COMMENT '联系地址',
-  `enabled` tinyint(1) DEFAULT '1',
-  `username` varchar(255) DEFAULT NULL COMMENT '用户名',
-  `password` varchar(255) DEFAULT NULL COMMENT '密码',
-  `userface` varchar(255) DEFAULT NULL,
+  `enabled` tinyint(1) DEFAULT '1' COMMENT '是否可用',
+  `username` varchar(64) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(50) DEFAULT NULL COMMENT '密码',
+  `salt` varchar(20) DEFAULT NULL COMMENT '盐加密',
+  `userface` varchar(128) DEFAULT NULL,
   `remark` varchar(255) DEFAULT NULL,
   `create_time` timestamp NULL DEFAULT NULL,
   `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
@@ -21,11 +22,11 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Table structure for role
 -- ----------------------------
-DROP TABLE IF EXISTS `role`;
-CREATE TABLE `role` (
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) DEFAULT NULL,
-  `name_zh` varchar(64) DEFAULT NULL COMMENT '角色名称',
+  `name` varchar(64) DEFAULT NULL COMMENT '角色名称',
+  `name_zh` varchar(64) DEFAULT NULL COMMENT '角色名称中文',
   `create_time` timestamp NULL DEFAULT NULL,
   `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
@@ -34,18 +35,17 @@ CREATE TABLE `role` (
 -- ----------------------------
 -- Table structure for menu
 -- ----------------------------
-DROP TABLE IF EXISTS `menu`;
-CREATE TABLE `menu` (
+DROP TABLE IF EXISTS `sys_menu`;
+CREATE TABLE `sys_menu` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `url` varchar(64) DEFAULT NULL,
-  `path` varchar(64) DEFAULT NULL,
-  `component` varchar(64) DEFAULT NULL,
-  `name` varchar(64) DEFAULT NULL,
-  `icon_cls` varchar(64) DEFAULT NULL,
-  `keep_alive` tinyint(1) DEFAULT NULL,
-  `require_auth` tinyint(1) DEFAULT NULL,
-  `parent_id` bigint(20) DEFAULT NULL,
-  `enabled` tinyint(1) DEFAULT '1',
+  `url` varchar(64) DEFAULT NULL COMMENT 'url',
+  `component` varchar(64) DEFAULT NULL COMMENT '菜单内容',
+  `name` varchar(64) DEFAULT NULL COMMENT '菜单名称',
+  `icon_cls` varchar(64) DEFAULT NULL COMMENT '图标',
+  `keep_alive` tinyint(1) DEFAULT NULL COMMENT '保活',
+  `require_auth` tinyint(1) DEFAULT NULL COMMENT '要求',
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '父菜单ID',
+  `enabled` tinyint(1) DEFAULT '1' COMMENT '是否可用',
   `create_time` timestamp NULL DEFAULT NULL,
   `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
@@ -55,11 +55,11 @@ CREATE TABLE `menu` (
 -- ----------------------------
 -- Table structure for hr_role
 -- ----------------------------
-DROP TABLE IF EXISTS `user_role`;
-CREATE TABLE `user_role` (
+DROP TABLE IF EXISTS `sys_user_role`;
+CREATE TABLE `sys_user_role` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `u_id` bigint(20) DEFAULT NULL,
-  `r_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL COMMENT '用户ID',
+  `role_id` bigint(20) DEFAULT NULL COMMENT '角色ID',
   `create_time` timestamp NULL DEFAULT NULL,
   `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
@@ -69,11 +69,11 @@ CREATE TABLE `user_role` (
 -- ----------------------------
 -- Table structure for menu_role
 -- ----------------------------
-DROP TABLE IF EXISTS `menu_role`;
-CREATE TABLE `menu_role` (
+DROP TABLE IF EXISTS `sys_menu_role`;
+CREATE TABLE `sys_menu_role` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `m_id` bigint(20) DEFAULT NULL,
-  `r_id` bigint(20) DEFAULT NULL,
+  `menu_id` bigint(20) DEFAULT NULL COMMENT '菜单ID',
+  `role_id` bigint(20) DEFAULT NULL COMMENT '角色ID',
   `create_time` timestamp NULL DEFAULT NULL,
   `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
